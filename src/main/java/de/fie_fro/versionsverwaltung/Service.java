119c16809b13888ba5417ea39b5899a1b9ee1a7a
Enrico <enricofroese@web.de>
2023-01-30 15:10:56 +0100

    Erweiterungen an ConsoleScanner + Methodenlayout vom Service

--- src/main/java/de/fie_fro/versionsverwaltung/Service.java
+++ src/main/java/de/fie_fro/versionsverwaltung/Service.java
@@ -1,16 +1,59 @@
 package de.fie_fro.versionsverwaltung;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class Service 
 {
-	public void main( String[] args )
-    {
-        System.out.println( "Hello World" );
-    }
+	int id;
+	public Service() {
+		DateFormat dateFormat = new SimpleDateFormat("ddHHmmss");
+        String date = dateFormat.format(new Date());
+ 
+        id = Integer.valueOf(date);
+	}
+	
 	public void compare(String pFilename1, String pFilename2) {
-		System.out.println(pFilename1 + " und " + pFilename2);
+		System.out.println("Vergleiche Dateien "+pFilename1+" und "+pFilename2+":");
+	}
+	
+	public void editFile(String pFilename) {
+		System.out.println("Bearbeite Datei "+pFilename);
+	}
+	
+	public String uploadNewFile(String pPathToFile) {
+		// TODO Auto-generated method stub
+		String dateiname = "";
+		int version = 0;
+		return "Neue Datei \""+dateiname+"\" wurde erfolgreich hochgeladen (Version "+version+").";
+	}
+	
+	public String setFileVersionToHead(String pFilename, String pVersion) {
+		int version = Integer.valueOf(pVersion);
+		System.out.println("Version: "+version);
+		// TODO Auto-generated method stub
+		return "";
+	}
+	
+	public void getFileVersionHistory(String pFilename) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void viewFile(String pFilename) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String uploadExistingFileWithNewVersion(String pPathToFile) {
+		// TODO Auto-generated method stub
+		String dateiname = "";
+		int version = 0;
+		return "Datei \""+dateiname+"\" wurde erfolgreich hochgeladen (Version "+version+").";
 	}
 }
--- src/main/java/de/fie_fro/versionsverwaltung/consoleScanner.java
+++ src/main/java/de/fie_fro/versionsverwaltung/consoleScanner.java
@@ -1,6 +1,7 @@
 package de.fie_fro.versionsverwaltung;
 
 import java.util.Scanner;
+//import java.io.
 
 public class consoleScanner {
 	
@@ -31,10 +32,11 @@ public class consoleScanner {
 		scanner.close();
 	}
 	
-	private static void writeConsole(String pInfo)
+	private static void writeConsole(String pInfo) //eigentlich obsolet
 	{
 		System.out.println(pInfo);
 	}
+	
 	private static void evaluateConsoleInput(String pInput)
 	{
 		String input[] = pInput.split(" ");
@@ -44,29 +46,72 @@ public class consoleScanner {
 		case "help":
 			writeConsole("comp\t2 Dateien vergleichen(Parameter: Dateiname Dateiname)\n"
 					+ "edit\tDatei bearbeiten (Parameter: Dateiname)\n"
-					+ "newf\tNeue Datei erstellen (Parameter: Dateiname)\n"
+					+ "newf\tNeue Datei erstellen (Parameter: Pfad zur Datei)\n"
 					+ "setv\tVersion als aktuell setzen (Parameter: Dateiname Version)\n"
 					+ "vhis\tVersionshistorie anzeigen (Parameter: Dateiname)\n"
 					+ "view\tDatei anzeigen (Parameter: Dateiname)\n"
 					+ "\tDatei der Version n anzeigen (Parameter: Dateiname Version)\n"
-					+ "upld\tDatei hochladen (Parameter: Dateiname)\n\n"
+					+ "upld\tDatei hochladen (Parameter: Pfad zur Datei)\n\n"
 					+ "Beispiel: edit MyApp.java");
+			//evtl. newf und upld zusammenlegen und dann im Service unterscheiden, 
+			//ob neue Datei oder neue Version
             break;
 		case "comp":
-			service.compare(input[1], input[2]);
-			//vorher abfangen, wie viele Parameter gesetzt sind
+			try {
+				service.compare(input[1], input[2]);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "edit":
+			try {
+				service.editFile(input[1]);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "newf":
+			try {
+				writeConsole(service.uploadNewFile(input[1]));
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "setv":
+			try {
+				writeConsole(service.setFileVersionToHead(input[1], input[2]));
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "vhis":
+			try {
+				service.getFileVersionHistory(input[1]);
+				//TODO Versionshistorie ausgeben
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "view":
+			try {
+				service.viewFile(input[1]);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case "upld":
+			try {
+				writeConsole(service.uploadExistingFileWithNewVersion(input[1]));
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 			break;
         default:
         	break;
